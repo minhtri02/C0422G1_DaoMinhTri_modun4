3e92a9171003c5fa1d346b18fba41f9e7d6460c8
unknown <you@example.com>
2022-08-16 10:57:48 +0700

    update

--- ss1_tong_quan_spring_MVC/exercise/change_money/src/main/java/com/example/controller/ChangeMoneyController.java
+++ ss1_tong_quan_spring_MVC/exercise/change_money/src/main/java/com/example/controller/ChangeMoneyController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.stereotype.Controller
-public class Controller {
+public class ChangeMoneyController {
 
     @Autowired
     private IChangeMoneyService iChangeMoneyService;
